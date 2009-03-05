b08bc65557fd44d9ca2cf0ba94cbe8d25aaf62f3
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-03-05 16:35:56 +0000

    Finally a prkl form

--- web/forms.py
+++ web/forms.py
@@ -2,5 +2,20 @@
 
 from django.db.transaction import commit_on_success
 
+from django import forms
+
+from web import models
+
+class SubmitPrklForm(forms.Form):
+    prkl = forms.CharField(label='Sinun prkleesi', widget=forms.widgets.TextInput())
+
+    @commit_on_success
+    def save(self):
+        new_prkl = models.Prkl()
+        new_prkl.content = self.cleaned_data['prkl']
+        if self.data['user'].id:
+            new_prkl.user = self.data['user']
+        new_prkl.save()
+
 # EOF
 
