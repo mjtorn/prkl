cd6164a22f09850dd8ac211b2cb08683f852fb8f
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-03-05 15:56:30 +0000

    Initial index

--- web/views.py
+++ web/views.py
@@ -1,6 +1,14 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+def index(request):
+    """Our index page
+    """
+
+    return HttpResponse('lol', content_type='text/plain')
 
 # EOF
 
