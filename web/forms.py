ab04a8e612a63db6c1d5a995b7f90ebab3a72d8e
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-03-05 16:44:11 +0000

    Initial cleaner for prkl content

--- web/forms.py
+++ web/forms.py
@@ -7,12 +7,25 @@ from django import forms
 from web import models
 
 class SubmitPrklForm(forms.Form):
-    prkl = forms.CharField(label='Sinun prkleesi', widget=forms.widgets.TextInput())
+    content = forms.CharField(label='Sinun prkleesi', widget=forms.widgets.TextInput())
+
+    def clean_content(self):
+        content = self.data['content']
+        print content
+
+        if not content.lower().startswith('tänään'):
+            raise forms.ValidationError('Aloitathan Prkleesi sanalla tänään')
+
+        # FIXME: Allow some amount of . and ! and maybe ? at the end
+        if not content.lower().endswith('prkl'):
+            raise forms.ValidationError('Päätäthän Prkleesi sanalla prkl')
+
+        return content
 
     @commit_on_success
     def save(self):
         new_prkl = models.Prkl()
-        new_prkl.content = self.cleaned_data['prkl']
+        new_prkl.content = self.cleaned_data['content']
         if self.data['user'].id:
             new_prkl.user = self.data['user']
         new_prkl.save()
