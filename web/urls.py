5120e80a9ae6f9bf5947c84b60d962c222457b19
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-03-05 18:44:48 +0000

    To serve javascript

--- web/urls.py
+++ web/urls.py
@@ -2,11 +2,22 @@
 
 from django.conf.urls.defaults import *
 
+from django.conf import settings
+
 import views
 
 urlpatterns = patterns('',
     (r'^top', views.top),
     (r'^bottom', views.bottom),
+)
+
+if settings.KLUDGE_STATIC:
+    urlpatterns += patterns('',
+    # Static
+    (r'^media/(?P<path>.*)/?$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
+
+urlpatterns += patterns('',
     (r'^', views.index),
 )
 
