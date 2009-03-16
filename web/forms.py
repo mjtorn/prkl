bc6cc6c8ace86dee01acde8bd2abdaae4b4174c2
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-03-16 18:56:38 +0000

    Registration form

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,7 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
 from django.contrib.auth import authenticate
+from django.contrib.auth import models as auth_models
 
 from django.db.transaction import commit_on_success
 
@@ -21,6 +22,49 @@ class LoginForm(forms.Form):
             raise forms.ValidationError('Nimi tai salasana väärä')
 
         return self.data['username']
+
+
+class RegisterForm(forms.Form):
+    # Custom Finnish error messages
+    error_messages = {
+        'required': 'Tämä kenttä tarvitaan',
+        'invalid': 'Epävalidi sähköpostiosoite',
+    }
+    reg_username = forms.CharField(label='Tunnus', error_messages=error_messages)
+    reg_password = forms.CharField(label='Salasana', widget=forms.widgets.PasswordInput(), error_messages=error_messages)
+    reg_email = forms.EmailField(label='Sähköposti', error_messages=error_messages)
+
+    def clean_reg_username(self):
+        username = self.data.get('reg_username', None)
+        print 'username', username
+        if not username:
+            raise forms.ValidationError('Tunnus tarvitaan')
+
+        exists = (auth_models.User.objects.filter(username=username).count() > 0)
+        if exists:
+            raise forms.ValidationError('Tunnus on jo otettu')
+
+        return username
+
+    def clean_reg_email(self):
+        email = self.data.get('reg_email', None)
+        if not email:
+            raise forms.ValidationError('Sähköposti tarvitaan')
+
+        exists = (auth_models.User.objects.filter(email=email).count() > 0)
+        if exists:
+            raise forms.ValidationError('Sähköposti on jo otettu')
+
+        return email
+
+    @commit_on_success
+    def save(self):
+        username = self.cleaned_data['reg_username']
+        password = self.cleaned_data['reg_password']
+        email = self.cleaned_data['reg_email']
+        user = auth_models.User.objects.create_user(username, email, password)
+        return user
+
 
 class SubmitPrklForm(forms.Form):
     content = forms.CharField(label='Sinun prkleesi', widget=forms.widgets.TextInput())
