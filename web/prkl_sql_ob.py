fa048ffeab0b85f5da8142d31027777c5181aad6
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-04-18 11:35:16 +0000

    Also likings

--- web/prkl_sql_ob.py
+++ web/prkl_sql_ob.py
@@ -10,6 +10,7 @@ class PrklQuery(object):
     RAW_QRY = """\
 SELECT p.id, p.content, p.score, p.created_at,
         %(vote_snippet_qry)s,
+        %(like_snippet_qry)s
         u.id, u.username,
         t.id, t.name
 FROM web_prkl p
@@ -28,6 +29,10 @@ NOT EXISTS(SELECT 1 FROM web_prklvote WHERE prkl_id=p.id AND user_id=%d)
 NOT EXISTS(SELECT 1 FROM web_prklvote WHERE prkl_id=p.id AND true_id='%s')
 """
 
+    LIKE_SNIPPET_USERID_QRY = """\
+NOT EXISTS(SELECT 1 FROM web_prkllike WHERE prkl_id=p.id AND user_id=%d)
+"""
+
     def __init__(self, **kwargs):
         # Everything related to the query here
         self.opts = {}
@@ -41,6 +46,14 @@ NOT EXISTS(SELECT 1 FROM web_prklvote WHERE prkl_id=p.id AND true_id='%s')
             raise KeyError('vote_userid or vote_trueid needed')
 
         self.opts['vote_snippet_qry'] = vote_snippet_qry
+
+        ## Does the user like this?
+        if kwargs.has_key('like_userid'):
+            like_userid = self.LIKE_SNIPPET_USERID_QRY % kwargs['like_userid']
+            # We always have vote_snippet_qry so we always need comma here
+            self.opts['like_snippet_qry'] = '%s, ' % like_userid
+        else:
+            self.opts['like_snippet_qry'] = ''
 
     def execute(self):
         qry = self.RAW_QRY % self.opts 
