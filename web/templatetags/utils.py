c7578abe6237382aaf1b0d1a866a8c9f6247ebc9
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-04-28 09:16:45 +0000

    Create properly formatted tag links

--- web/templatetags/utils.py
+++ web/templatetags/utils.py
@@ -1,5 +1,8 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django.utils.html import escape
+from django.utils.safestring import SafeUnicode
+
 from django.template import Library
 
 register = Library()
@@ -20,6 +23,20 @@ def check_active(req_path, path):
 @register.filter
 def prkltagjoin(tags, joiner):
     return joiner.join([t['name'] for t in tags])
+
+@register.filter
+def prkltaglinks(tags, request):
+    bare_href = '<a href="http://%s/%s" class="prkl_tag">%s</a>' % (request.META['HTTP_HOST'], '%s', '%s')
+
+    tag_links = [bare_href % (escape(t['name'].lower()), escape(t['name'])) for t in tags]
+    out_links = ', '.join(tag_links)
+
+    # Somehow setting prkltaglinks.is_safe = True does not help
+#    out = '<span>%s</span>' % out_links
+    out = SafeUnicode('<span>%s</span>' % out_links)
+
+    return out
+#prkltaglinks.is_safe = True
 
 # EOF
 
