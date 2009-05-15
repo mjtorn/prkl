0da8e9ae56ffadba84fec010138cfc89cc794f2f
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-15 20:17:54 +0000

    Enable csrf checking for posts

--- settings.py
+++ settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
 )
 
 if DEBUG_TOOLBAR:
