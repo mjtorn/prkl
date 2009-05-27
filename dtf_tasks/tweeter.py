119d267fc0069917f1c1a2ef79d57f3d1d87e0d7
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-27 11:10:26 +0000

    More infra for tweeting

--- dtf_tasks/tweeter.py
+++ dtf_tasks/tweeter.py
@@ -10,6 +10,7 @@ from prkl import dqs_settings
 
 import datetime
 
+import urllib
 import urllib2
 
 class Tweeter(BaseTask):
@@ -30,20 +31,46 @@ class Tweeter(BaseTask):
 
         return tweet
 
-    def soft_delete(self):
-        pass
+    def soft_delete(self, queue_name, message_id):
+        dqs_connstring = 'http://%s:%s' % (dqs_settings.DQS_HOST, dqs_settings.DQS_PORT)
+        dqs_connstring = '%s/q/%s/delete/' % (dqs_connstring, queue_name)
+        data = {
+            'message_id': message_id,
+            'soft': 'true'
+        }
+        try:
+            conn = urllib2.urlopen(dqs_connstring, urllib.urlencode(data))
+        except IOError, msg:
+            raise TaskFailed(msg)
+
+        res = conn.read()
+        print 'res', res
+
+    def do_tweet(self, tweet):
+        # Hate returning booleans
+        return True
 
     def run(self, *args, **kwargs):
         # Canary bailout
         started_at = datetime.datetime.now()
         #max_run = datetime.timedelta(minutes=3)
-        max_run = datetime.timedelta(seconds=10)
+        max_run = datetime.timedelta(seconds=5)
 
         self.progress = 'Started'
         self.results = {}
 
         try:
             tweet = self.get_tweet('twitter')
+
+            # Queue empty
+            if not tweet:
+                self.progress = 'Done'
+                self.results = {
+                    'status': 'QUEUEEMPTY',
+                    'tweet': None,
+                }
+                return self.results
+
         except TaskFailed, msg:
             self.progress = 'TaskFailed: %s' % msg
             raise
@@ -52,7 +79,6 @@ class Tweeter(BaseTask):
 
         success = False
         while not success:
-            time.sleep(1)
             self.progress = 'Tweeting id %s' % tweet['id']
             self.results = {
                 'status': 'RUNNING',
@@ -63,9 +89,15 @@ class Tweeter(BaseTask):
             if datetime.datetime.now() >= started_at + max_run:
                 break
 
+            time.sleep(1)
+
+            success = self.do_tweet(tweet['message'])
+
         if not success:
             self.progress = 'TASKTIMEOUT'
             raise TaskFailed('Task timed out')
+        else:
+            self.soft_delete('twitter', tweet['id'])
 
         return self.results
 
