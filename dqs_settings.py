d3bfbd71fe7d85fae8117e979da8bdf0f5fdcb35
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-27 08:40:55 +0000

    Queue conf

---
+++ dqs_settings.py
@@ -0,0 +1,11 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'prkl'
+
+DQS_HOST = '83.150.88.181'
+DQS_PORT = 8021
+DQS_SERVER_NAME = 'fadconsulting.com'
+
+# EOF
+
