e5ae48ca061261b1e0f6774e3aec14f394fc630e
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-27 11:52:21 +0000

    Put server in localhost and prep twit auth

--- taskforce_settings.py
+++ taskforce_settings.py
@@ -1,6 +1,6 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
-TASKFORCE_SERVER = '83.150.88.181:8016'
+TASKFORCE_SERVER = '127.0.0.1:8016'
 
 INSTALLED_APPS = (
     'prkl.dtf_tasks',
@@ -9,6 +9,9 @@ INSTALLED_APPS = (
 ## Until we have a cleanup method, just don't care
 #ALLOW_TASK_OVERRIDE = False
 ALLOW_TASK_OVERRIDE = True
+
+TWITTER_USERNAME = ''
+TWITTER_PASSWORD = ''
 
 # EOF
 
