355d369ad83828251ff0d3700665ff79f1cc2973
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-28 19:56:36 +0000

    Clean the code up

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -20,21 +20,12 @@ class TrueIdMiddleware(object):
         return true_id
 
     def process_request(self, request):
-        # Did we just fake an entry in cookies?
         request.has_session = (request.session.session_key == request.COOKIES.get('sessionid', ''))
+
+        ## Create true_id_ob or pull it out of the database
         if not request.COOKIES.has_key('true_id'):
             true_id = self.gen_true_id()
             true_id_ob = models.TrueId.objects.create(hash=true_id)
-
-            # Move this below to process_response
-            """
-            if not request.has_session:
-                request.COOKIES['true_id'] = true_id
-            else:
-                response = HttpResponseRedirect(request.META['PATH_INFO'])
-                response.set_cookie('true_id', true_id_ob.hash, max_age=(2**32)-1, domain=settings.COOKIE_DOMAIN)
-                return response
-            """
         else:
             true_id = request.COOKIES['true_id']
 
@@ -47,22 +38,15 @@ class TrueIdMiddleware(object):
 
         request.true_id = true_id_ob
 
-#        print 'cookies', request.COOKIES['true_id']
-        print 'request.true_id', request.true_id
+        # By returning None we are able to re-use the request in views
+        return None
 
     def process_response(self, request, response):
-        print 'response', id(response)
 
-        ## WTF
-        #if not request.has_session:
-        #    request.COOKIES['true_id'] = true_id
-        #else:
-        #if True:
+        ## If we didn't have a true id cookie in process_request, set it here
         if not request.COOKIES.has_key('true_id'):
-            print 'wtf here'
-            #response = HttpResponseRedirect(request.META['PATH_INFO'])
             response.set_cookie('true_id', request.true_id.hash, max_age=(2**32)-1, domain=settings.COOKIE_DOMAIN)
-            return response
+
         return response
 
 # EOF
