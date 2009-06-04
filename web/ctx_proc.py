a99318a8bb2888e6f221b7342e3b417d63d37d72
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-06-04 15:11:36 +0000

    Determine, in context, if we are in production or not

--- web/ctx_proc.py
+++ web/ctx_proc.py
@@ -1,8 +1,15 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django.conf import settings as django_settings
+
 def strip_path(request):
     return {
         'strip_path': request.META['PATH_INFO'].strip('/')
+    }
+
+def settings(request):
+    return {
+        'production': django_settings.PRODUCTION,
     }
 
 # EOF
